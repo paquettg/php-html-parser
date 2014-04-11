84b3440d4d0ed33bc3b389374460ecf25d198e26
Gilles Paquette <paquettg@gmail.com>
2014-04-11 13:39:33 -0400

    Added appropriat Options tests

---
+++ tests/OptionsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPHtmlParser\Options;
+
+class OptionsTest extends PHPUnit_Framework_TestCase {
+
+	public function testDefaultWhitespaceTextNode()
+	{
+		$options = new Options;
+
+		$this->assertTrue($options->whitespaceTextNode);
+	}
+
+	public function testAddingOption()
+	{
+		$options = new Options;
+		$options->setOptions([
+			'test' => true,
+		]);
+
+		$this->assertTrue($options->test);
+	}
+
+	public function testAddingOver()
+	{
+		$options = new Options;
+		$options->setOptions([
+			'test' => false,
+		])->setOptions([
+			'test' => true,
+			'whitespaceTextNode' => false,
+		]);
+
+		$this->assertFalse($options->get('whitespaceTextNode'));
+	}
+}
+
