f2966ad5e287ed5435484e4472aa139faf51474e
Tomáš Fedor <tomas.fedor3@gmail.com>
2020-01-25 19:36:43 +0100

    Throw exception when trying to set unknown option

---
+++ src/PHPHtmlParser/Exceptions/UnknownOptionException.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+namespace PHPHtmlParser\Exceptions;
+
+use Exception;
+
+/**
+ * Class UnknownOptionException
+ *
+ * @package PHPHtmlParser\Exceptions
+ */
+final class UnknownOptionException extends Exception
+{
+}
